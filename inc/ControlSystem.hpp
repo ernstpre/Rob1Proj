4b1b47053b0cea677e3dd96374da013618cf0d25
Preston <ernstpre@students.zhaw.ch>
2021-05-10 20:35:09 +0200

    first trail to comit ex3_1 from VSCode

--- inc/ControlSystem.hpp
+++ inc/ControlSystem.hpp
@@ -4,7 +4,6 @@
 #include <eeros/control/TimeDomain.hpp>
 #include <eeros/core/Executor.hpp>
 #include <eeros/control/PeripheralInput.hpp>
-#include <eeros/control/Constant.hpp>
 #include <eeros/control/Gain.hpp>
 #include <eeros/control/Saturation.hpp>
 #include <eeros/control/PeripheralOutput.hpp>
@@ -19,9 +18,8 @@ public:
     // Define Blocks
     PeripheralInput<> E1,E2;
     Gain<> scale1, i1, kM1;
-    Saturation<> qdmax1;
+    Saturation<> qdMax1;
     PeripheralOutput<> M1;
-    Constant<> myConstant;
     
     TimeDomain timedomain;
 };
